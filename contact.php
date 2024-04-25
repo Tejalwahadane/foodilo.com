8df208ada387191e8628d7702f422b51ad40d147
Tejalwahadane <160157255+Tejalwahadane@users.noreply.github.com>
2024-04-25 16:22:59 +0530

    Update contact.php

--- contact.php
+++ contact.php
@@ -3,9 +3,14 @@
     <link rel="stylesheet" href="contact.css">
 </head>
 
-
-
-
-
+<body>
+    <div id="main" style="border: 5px solid black; background-color: grey; width: 900px; margin-left: 20%; font-size: larger;">
+        <h2 style="font-style: italic ;">For any query contact on:</h2>
+           <div id="c5">Our Official Address:- pune wagholi</div>
+           <div id="c2">General Enquiries:- Fodilo@gmail.com</div>
+           <div id="c3">call Us:-97xxxxxx54</div>
+           <div id="c4">Our Timing:- Mon- Sun : 6:30AM - 11:00PM</div>
+    </div>
+</body>
 
 <?php include('partials-front/footer.php'); ?>
