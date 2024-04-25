1b640a3b096d49865d559d9a5856a7ca1d3e87bc
Tejalwahadane <160157255+Tejalwahadane@users.noreply.github.com>
2024-04-25 16:32:32 +0530

    Update about_us.php

--- about_us.php
+++ about_us.php
@@ -3,7 +3,21 @@
     <link rel="stylesheet" href="about_us.css">
 </head>
 
+<div id="main" style=" border: 5px solid black; margin-left: 25%; width: 700px; height: 75vh; font-size: larger; background-color: rgb(181, 181, 235);">
+        <div id="info" style="color: black;">
+            Foodilo.com is your one-stop shop for all things food! We're passionate about making delicious and convenient food options accessible to everyone. Whether you're craving a quick lunch, planning a large gathering, or hosting a special event, Foodilo has something for you.
+        </div>
+        <h2>Our story</h2>
+        <div id="story">
+            In 2016, a team of passionate food lovers in Pune, India, saw a growing need for a convenient way to access delicious, high-quality meals. Tired of limited options and impersonal service, they envisioned a platform that celebrated the culinary heritage of India while embracing the fast-paced modern lifestyle.
 
+Foodilo.com was born from this vision. It started as a way to connect home cooks, brimming with secret family recipes, with busy professionals yearning for a taste of home. The aroma of simmering curries and the sizzle of fresh naan wafted from kitchens across the city, delivered straight to customers' doorsteps.
+
+As Foodilo gained popularity, it expanded to include established local restaurants and cafes. This ensured a wider variety of cuisines, from regional specialties to contemporary takes on Indian classics. Foodilo became a one-stop shop for online food ordering, offering convenience without sacrificing quality or the warmth of homemade flavors.
+
+Today, Foodilo.com remains dedicated to its original mission: providing a seamless and satisfying food experience. It's a platform that celebrates India's vibrant culinary scene, one delicious dish at a time.
+        </div>
+     </div>
 
 
 
